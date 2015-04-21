e735322f4262105e2a9bc65c1f0efa15e5c7805a
unknown <r202-stu@AI241LAB0202P20.aipdcls.edu>
2015-04-20 18:03:33 -0700

    updated week 2 project

--- CG_Programming_aFletcher/application.h
+++ CG_Programming_aFletcher/application.h
@@ -6,5 +6,25 @@
 #include <iostream>
 #include <cassert>
 #include <stdexcept>
+#include <glew.h>
+#include <glfw3.h>
+#include <glm/glm.hpp> 
+#include <memory>
+
 
 using namespace std;
+using namespace glm;
+
+#define GLFW_DLL
+#define APP_NAME "<<Fletcher>>"
+#define EXIT_WITH_ERROR -1
+#define	EXIT_WITH_SUCCESS 0
+#define OPEN_GL_VERSION 3
+#define ANTIALIASING 4
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 768
+#define GLFW_FAIL 0
+
+extern GLFWwindow* window;
+extern GLint shaderStatus;
+extern bool activeRef;
--- CG_Programming_aFletcher/main.cpp
+++ CG_Programming_aFletcher/main.cpp
@@ -1,13 +1,59 @@
 #include "application.h"
 
-int main () {
+GLFWwindow* window = NULL;
 
-	cout << "Hello" << endl;
+int InitWindowFailed() {
+	if(glfwInit() == GLFW_FAIL){
+		fprintf(stderr, "failed to initialize GLFW\n");
+		return EXIT_WITH_ERROR;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, ANTIALIASING);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, OPEN_GL_VERSION);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, OPEN_GL_VERSION);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  //Gets working on MAC
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // we don't want old open GL
+
+	window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, APP_NAME, NULL, NULL);
+
+	if(window == NULL) {
+		fprintf(stderr, "failed to create/open GLFW window.\n");
+		glfwTerminate();
+		return EXIT_WITH_ERROR;
+	}
+	
+	glfwMakeContextCurrent(window);
+
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+		
+	return EXIT_WITH_SUCCESS;
+}
+
+int InitGlewFailed(){
+	glewExperimental = true;   //Has to do with core profile
+	int init = (int)glewInit();
+	if(glewInit() != GLEW_OK) {
+		fprintf(stderr, "failed to initialize GLEW\n");
+		return EXIT_WITH_ERROR;
+	}
+
+	return EXIT_WITH_SUCCESS;
+}
+
+int main () {
+	if(InitWindowFailed() | InitGlewFailed()){
+		return EXIT_WITH_ERROR;
+	}
 
 	do{
+		//update();
+		//render();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}while(glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		glfwWindowShouldClose(window) == 0);
 
-	}while(true);
-
+	return EXIT_WITH_SUCCESS;
 
 
 }
